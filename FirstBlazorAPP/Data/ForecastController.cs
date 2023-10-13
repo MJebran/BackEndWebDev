0f8fe9e31bc5b48c428ee006ca98108c089b37f7
MJebran <jebran.mustafa@gmail.com>
2023-10-13 04:25:36 -0600

    watching the video and following till num7

---
+++ FirstBlazorAPP/Data/ForecastController.cs
@@ -0,0 +1,25 @@
+﻿/*
+using FirstBlazorApp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasksl
+
+
+namespace FirstBlazorApp
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ForecastController : ControllerBase
+    {
+        public async Task<ActionResult<IEnumerable<WeatherForecast>>> GetTask
+        {
+            var svc = new WeatherForecastService();
+            return new OkObjectResult(await svc.GetForecastAsync(DateTime.))
+        }
+    }
+}
+
+*/
